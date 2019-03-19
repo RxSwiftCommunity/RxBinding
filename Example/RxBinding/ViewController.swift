8036039e80cbd87dee52a0b70a0a3aad1b990052
Meng Li <lm2343635@126.com>
2019-03-19 21:09:04 +0900

    Remove storyboard.

--- Example/RxBinding/ViewController.swift
+++ Example/RxBinding/ViewController.swift
@@ -2,23 +2,29 @@
 //  ViewController.swift
 //  RxBinding
 //
-//  Created by lm2343635 on 03/14/2019.
-//  Copyright (c) 2019 lm2343635. All rights reserved.
+//  Created by Meng Li on 03/14/2019.
+//  Copyright (c) 2019 MuShare. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let viewModel: ViewModel
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 }
-
---
+++ Example/RxBinding/ViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  ViewModel.swift
+//  RxBinding_Example
+//
+//  Created by Meng Li on 2019/03/19.
+//  Copyright © 2019 MuShare. All rights reserved.
+//
+
+class ViewModel {
+    
+}
